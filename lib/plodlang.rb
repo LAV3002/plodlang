0e4de41e11db0916db90fde24c11d9dbcbb7a62b
LAV3002 <l1khachevAV@yandex.ru>
2026-01-19 18:09:56 +0300

    init

---
+++ lib/plodlang.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "plodlang/version"
+
+module Plodlang
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/plodlang_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Plodlang do
+  it "has a version number" do
+    expect(Plodlang::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
